f7ba7be6b2ae9d73e536c3d403817ea5fb544d68
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-03-08 17:58:31 +0530

    minor changes

--- src/components/Products Page/NoResultsFound.jsx
+++ src/components/Products Page/NoResultsFound.jsx
@@ -3,7 +3,9 @@ import { Box } from "@mui/system";
 
 export default function NoResultsFound(){
     return <Box width={"70vw"}>
+        <Box>
         <Typography variant="h5" fontSize={"40px"}>Sorry :(</Typography>
         <Typography variant="h6" fontSize={"40px"}>No Results Found</Typography>
+        </Box>
     </Box>
 }
