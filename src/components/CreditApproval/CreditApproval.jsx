cb0c90eff50db44865dc8fad470982aa11090007
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-03-13 19:38:28 +0530

    credit page edited

--- src/components/CreditApproval/CreditApproval.jsx
+++ src/components/CreditApproval/CreditApproval.jsx
@@ -1,5 +1,8 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 export default function CreditApproval(){
-    return <Box>Credit Approval</Box>
+    return <Box>
+        <Typography>Credit Approval</Typography>
+    </Box>
 }
